ee51fd6a2c22128606dd3afce23b3cdc14835be8
Mr. Davydov <evreqyeahme@gmail.com>
2021-09-09 00:27:26 +0300

    Алгоритм обмена

--- 3.cpp
+++ 3.cpp
@@ -5,11 +5,12 @@ using namespace std;
 int main()
 {
 	int x = 9, y = 5;
+	cout << "Введите поочередно значения x и y:" << endl;
 	cin >> x >> y;
 	x = x - y;  //x = 4
 	y = y + x;  //y = 9
 	x = y - x;  //x = 5
-	cout << x << endl << y << endl;
+	cout << "x = " << x << endl << "y = " << y << endl;
 	return 0;
 }
 
