da5949d09163f90c97c970d6b5213618f24aec68
Mr. Davydov <evreq@MacBook-Pro-13.local>
2021-09-09 00:15:18 +0300

    Алгоритм обмена

---
+++ 3.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int x = 9, y = 5;
+	cin >> x >> y;
+	x = x - y;  //x = 4
+	y = y + x;  //y = 9
+	x = y - x;
+	cout << x << endl << y << endl;
+	return 0;
+}
+
