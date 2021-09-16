5e4dfc7f411002e18b89d5a5bb9b62376203e6f4
Mr. Davydov <evreqyeahme@gmail.com>
2021-09-16 20:22:18 +0300

    Сортировка вставками

--- 2ned/2.cpp
+++ 2ned/2.cpp
@@ -7,19 +7,21 @@ using namespace std;
 int main()
 {
 	int n;
-	vector <double> vec;
-	cout << "Введите количество элементов массива" << endl;
-	cin >> n;
 	double x;
+	vector <double> vec;
+	cout << "Введите количество элементов массива:" << endl;
+	cin >> n;
+	cout << "Введите элементы массива:" << endl;
 	for (int i = 0; i < n; i++) {
 		cin >> x;
 		vec.push_back(x);
-	}
+	} // вектор готов
 	for (int i = 1; i < n; i++) {
 		for (int j = i; j != 0 && vec[j] < vec[j-1]; j--) {
 			swap(vec[j], vec[j-1]);
 		}
 	}
+	cout << "Отсортированный массив выглядит так:" << endl;
 	for (int i = 0; i < n; i++) {
 		cout << vec[i] << " ";
 	}
