02aa43ef3b54937b563f2896c09487021320eda1
Mr Davydov <evreq@Mrs-MacBook-Pro.local>
2021-11-11 19:46:39 +0300

    2

---
+++ 9ned/varia.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <new>
+
+template<typename T, typename... Args>
+auto coll(Args... args)
+{
+    int n = sizeof...(args);
+    T *pala = new T[n] {args...};
+    return pala;
+}
+
+void printArray(int arr[], int n)
+{
+    for (int i = 0; i < n; ++i)
+        std::cout << arr[i] << " ";
+    std::cout << "\n";
+}
+
+int main()
+{
+    int opo[] = { 2, 1 ,3 };
+    int *p = coll<int>(opo);
+    printArray(p, 3);
+    //std::cout << coll<int>(opo) << std::endl;
+    return 0;
+}
