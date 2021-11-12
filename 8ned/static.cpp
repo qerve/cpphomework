0e8fe87b7faed21e7839f526071d1a5a1a2596f4
Mr Davydov <evreq@Mrs-MacBook-Pro.local>
2021-11-12 20:11:16 +0300

    modified

--- 8ned/static.cpp
+++ 8ned/static.cpp
@@ -1,48 +1,66 @@
 #include <iostream>
+#include <algorithm>
 
-template <class T>
-void heapify(T& arr, int n, int i) {
+template<typename T>
+bool compare(T a, T b, bool flag)
+{
+    if (flag)
+        return a > b;
+    else
+        return a < b;
+}
+
+template <typename T>
+void heapify(T& arr, int n, int i, bool flag) 
+{
     int largest = i;
     int l = 2 * i + 1;
     int r = 2 * i + 2;
 
-    if (l < n && arr[l] > arr[largest])
+    if (l < n && compare(arr[l], arr[largest], flag))
         largest = l;
 
-    if (r < n && arr[r] > arr[largest])
+    if (r < n && compare(arr[r], arr[largest], flag))
         largest = r;
 
     if (largest != i) {
         std::swap(arr[i], arr[largest]);
 
-        heapify(arr, n, largest);
+        heapify(arr, n, largest, flag);
     }
 }
 
-template <class T, size_t N>
-void heapSort(int(&arr)[N]) {
+template <typename T, size_t N>
+void heapSort(T(&arr)[N], bool flag = true) {
     for (int i = N / 2 - 1; i >= 0; i--)
-        heapify(arr, N, i);
+        heapify(arr, N, i, flag);
 
     for (int i = N - 1; i >= 0; i--) {
         std::swap(arr[0], arr[i]);
 
-        heapify(arr, i, 0);
+        heapify(arr, i, 0, flag);
     }
 }
 
-void printArray(int arr[], int n) {
+template <typename T >
+void printArray(T  arr[], int n) {
     for (int i = 0; i < n; ++i)
         std::cout << arr[i] << " ";
     std::cout << "\n";
 }
 
 int main() {
-
+    std::cout << "Отсортировать по возрастанию или убыванию?\n1 - по возрастанию, 0 - по убыванию." << std::endl;
+    int optiom;
+    std::cin >> optiom;
+    char epp[] = { 'a', 'y', 'e', 'c', 'g', 'u' };
     int arr[] = { 12, 11, 13, 5, 6, 7 };
-    heapSort<int, 6>(arr);
-    printArray(arr, 6);
-
+    heapSort(arr, bool(optiom));
+    heapSort(epp, bool(optiom));
+    printArray(arr, 6);  // int
+    printArray(epp, 6);  // char
+    //std::cout << bool(optiom);
     return 0;
 }
+
 
