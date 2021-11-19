501f9a9f6b0647b40a62f237fd56ea8df65ef2ac
Mr Davydov <evreq@Mrs-MacBook-Pro.local>
2021-11-19 20:14:39 +0300

    decay

---
+++ 10+1ned/3.cpp
@@ -0,0 +1,111 @@
+#include <iostream>
+
+template< class T > struct remove_reference      {typedef T type;};
+template< class T > struct remove_reference<T&>  {typedef T type;};
+template< class T > struct remove_reference<T&&> {typedef T type;};
+
+template<bool B, class T, class F>
+struct conditional { typedef T type; };
+ 
+template<class T, class F>
+struct conditional<false, T, F> { typedef F type; };
+
+template<class T>
+struct is_array : std::false_type {};
+ 
+template<class T>
+struct is_array<T[]> : std::true_type {};
+ 
+template<class T, std::size_t N>
+struct is_array<T[N]> : std::true_type {};
+
+template<class T>
+struct remove_extent { typedef T type; };
+ 
+template<class T>
+struct remove_extent<T[]> { typedef T type; };
+ 
+template<class T, std::size_t N>
+struct remove_extent<T[N]> { typedef T type; };
+
+template<class T>
+struct is_function;
+template<typename>
+struct is_function : std::false_type {};
+template<class Ret, class... Args>
+struct is_function<Ret(Args...)> : std::true_type {};
+template<class Ret, class... Args>
+struct is_function<Ret(Args...) const> : std::true_type {};
+template<class Ret, class... Args>
+struct is_function<Ret(Args...) volatile> : std::true_type {};
+template<class Ret, class... Args>
+struct is_function<Ret(Args...) const volatile> : std::true_type {};
+template<class Ret, class... Args>
+struct is_function<Ret(Args......) const> : std::true_type {};
+template<class Ret, class... Args>
+struct is_function<Ret(Args......) volatile> : std::true_type {};
+template<class Ret, class... Args>
+struct is_function<Ret(Args......) const volatile> : std::true_type {};
+
+
+namespace detail {
+ 
+template <class T>
+struct type_identity { using type = T; };
+template <class T>
+auto try_add_pointer(int) -> type_identity<typename std::remove_reference<T>::type*>;
+template <class T>
+auto try_add_pointer(...) -> type_identity<T>;
+ 
+}  
+template <class T>
+struct add_pointer : decltype(detail::try_add_pointer<T>(0)) {};
+
+template< class T > struct remove_cv                   { typedef T type; };
+template< class T > struct remove_cv<const T>          { typedef T type; };
+template< class T > struct remove_cv<volatile T>       { typedef T type; };
+template< class T > struct remove_cv<const volatile T> { typedef T type; };
+ 
+template< class T > struct remove_const                { typedef T type; };
+template< class T > struct remove_const<const T>       { typedef T type; };
+ 
+template< class T > struct remove_volatile             { typedef T type; };
+template< class T > struct remove_volatile<volatile T> { typedef T type; };
+
+template< class T >
+struct decay {
+private:
+    typedef typename std::remove_reference<T>::type U;
+public:
+    typedef typename std::conditional< 
+        std::is_array<U>::value,
+        typename std::remove_extent<U>::type*,
+        typename std::conditional< 
+            std::is_function<U>::value,
+            typename std::add_pointer<U>::type,
+            typename std::remove_cv<U>::type
+        >::type
+    >::type type;
+};
+
+template<class T, class U>
+struct is_same : std::false_type {};
+ 
+template<class T>
+struct is_same<T, T> : std::true_type {};
+
+template <typename T, typename U>
+struct decay_equiv : 
+    is_same<typename decay<T>::type, U>::type 
+{};
+ 
+int main()
+{
+    std::cout << std::boolalpha
+              << decay_equiv<int, int>::value << '\n'
+              << decay_equiv<int&, int>::value << '\n'
+              << decay_equiv<int&&, int>::value << '\n'
+              << decay_equiv<const int&, int>::value << '\n'
+              << decay_equiv<int[2], int*>::value << '\n'
+              << decay_equiv<int(int), int(*)(int)>::value << '\n';
+}
